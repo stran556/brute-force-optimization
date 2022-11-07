e76bdf99373cf7fb10dced0b9931b0fab2b2b987
Sean <36829049+stran556@users.noreply.github.com>
2022-11-07 16:26:16 -0600

    Update sforce.py

--- bruteforce/sforce.py
+++ bruteforce/sforce.py
@@ -3,21 +3,21 @@ import time
 import math
 
 # single thread
-num = "99999999"
-num_hash = hashlib.sha256(str.encode(num)).hexdigest()
-print("Hash = " + num_hash)
+num_hash = input("Hash: ")
+digit = int(input("Number of digits to check: "))
+digit_pow = 10 ** (digit)
 
 counter = 0
+val = 0
 start = time.time()
-while True:
+while counter <= digit_pow:
     str_c = str(counter)
     if hashlib.sha256(str.encode(str_c)).hexdigest() == num_hash:
+        val = counter
         break
     counter = counter + 1;
 end = time.time()
 
 net = round(end - start, 2)
 print("Hash value = " + str(f'{counter:,}'))
-print("Time = " + str(net) + " sec \nHash/sec = " + str(f'{math.trunc(int(num) / (end - start)):,}'))
-
-# multi thread
+print("Time = " + str(net) + " sec \nHash/sec = " + str(f'{math.trunc(int(val) / (end - start)):,}'))
